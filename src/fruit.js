a9e8fd03a7a6da8460dc62ade0601d444c3e8efa
whatever <matt@svv.im>
2018-05-28 15:22:50 -0400

    :strawberry:

--- src/fruit.js
+++ src/fruit.js
@@ -1,11 +1,18 @@
 import {SquareGrid} from './square-grid.js';
 
+
+const CHERRIES_SOURCE = "/cherry.png";
+let CHERRIES_IMAGE = new Image();
+CHERRIES_IMAGE.src = CHERRIES_SOURCE;
+
 /**
  * Render
  */
-export function renderFruit(el, x, y) {
+export function renderFruit(el, x, y, dx, dy) {
   let ctx = el.getContext("2d");
+  ctx.clearRect(0, 0, el.width, el.height);
 
+  console.log(x, y, dx, dy);
 
   console.log("[fruits] [w, h] =", [el.width, el.height]);
 
@@ -14,25 +21,27 @@ export function renderFruit(el, x, y) {
 
   let grid = new SquareGrid({width: w, height: h});
 
-  ctx.transform(1, 0, 0, -1, 0, el.height);
+  // ctx.transform(1, 0, 0, -1, 0, el.height);
+  ctx.imageSmoothingEnabled = true;
+  ctx.imageSmoothingQuality = "high";
+
 
   for (let i=0; i < x; i++) {
     for (let j=0; j < y; j++) {
 
       let [x, y] = grid.getBoxCenter(i, j);
 
-      let x0 = Math.floor(x-4.5);
-      let x1 = Math.ceil(x+4.5);
-      let y0 = Math.floor(y-4.5);
-      let y1 = Math.ceil(y+4.5);
+      let x0 = Math.floor(x-dx);
+      let x1 = Math.floor(x+dx);
+      let y0 = Math.floor(y-dy);
+      let y1 = Math.floor(y+dy);
 
-      if ((i+j)%2) {
+      if ((i+j) % 2) {
         ctx.fillStyle = "green";
       } else {
         ctx.fillStyle = "black";
+        ctx.drawImage(CHERRIES_IMAGE, x0, y0, x1-x0, y1-y0);
       }
-
-      ctx.fillRect(x0, y0, x1-x0, y1-y0);
 
     }
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {SquareGrid} from './square-grid.js';
 import {renderFruit} from './fruit.js';
+import {getOffset, getViewport} from './hacks.js';
 
 
 /**
@@ -26,14 +27,18 @@ export function throttle(ms, f) {
   };
 }
 
-let topEl = {};
-let botEl = {}
-let fruitEl = {};
+var topEl = {};
+var botEl = {}
+var fruitEl = {};
 
 /**
  * Initiate ...
  */
 export function afterLoad(ev) {
+  knav.render(params.x, params.y, params.xspacing, params.yspacing);
+}
+
+export function render(x, y, w, h) {
   topEl = document.getElementById("knav");
   botEl = document.getElementById("knav-bottom");
   fruitEl = document.getElementById("fruits");
@@ -43,14 +48,15 @@ export function afterLoad(ev) {
 
   window.addEventListener('scroll', scroll);
 
-  renderFruit(fruitEl, 10, 10);
+  renderFruit(fruitEl, x, y, w, h);
 }
-
 
 /**
  * Set KNAV to floating on bottom mode
  */
 function setFloating() {
+  let botEl = document.getElementById("knav-bottom");
+  botEl.className = "floating";
   console.log("[Floating]");
 }
 
@@ -58,6 +64,8 @@ function setFloating() {
  * Set KNAV to sticking to bottom mode
  */
 function setSticking() {
+  let botEl = document.getElementById("knav-bottom");
+  botEl.className = "sticking";
   console.log("[Sticking]");
 }
 
@@ -65,8 +73,11 @@ function setSticking() {
 /**
  * Set state of nav
  */
-let scroll = throttle(333, function (ev) {
-  if (pageYOffset > topEl.offsetHeight) {
+let scroll = throttle(0, function (ev) {
+  let bot = getOffset(document.getElementById("complete-bottom"));
+  let vp = getViewport();
+  console.log(bot, vp);
+  if (pageYOffset > topEl.offsetHeight && pageYOffset < bot.top - vp[1]) {
     setFloating();
   } else {
     setSticking();
